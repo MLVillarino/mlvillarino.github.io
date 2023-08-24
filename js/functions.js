ad4682b88d041216a69b5f26eadea2fb6dbc20b2
MLVillarino <40810547+MLVillarino@users.noreply.github.com>
2023-08-24 12:27:14 +0900

    updates to scripts for mobile and desktop

---
+++ js/functions.js
@@ -0,0 +1,150 @@
+winWidth = 0;
+mobileMode = false;
+langMode = "en"; //check translation.js for the rest
+
+winWidth = $(window).width();
+chk = mobileCheck();
+if(chk){
+	mobileMode = true;
+	$("#menu").css("margin-left","110vw");
+	$("#menu").css("display","none");
+	$("#menu").css("opacity","0");
+	console.log("MOBILE MODE");
+}else{
+	mobileMode = false;
+	$("#menu").css("margin-left","0px");
+	$("#menu").css("display","flex");
+	$("#menu").css("opacity","100");
+	console.log("DESKTOP MODE");
+}
+
+$(window).resize(function(){ //FIX THIS
+	winWidth = $(window).width();
+	chk = mobileCheck();
+	if(chk){
+		mobileMode = true;
+		$("#menu").css("margin-left","110vw");
+		$("#menu").css("display","none");
+		$("#menu").css("opacity","0");
+	}else{
+		mobileMode = false;
+		$("#menu").css("margin-left","0px");
+		$("#menu").css("display","flex");
+		$("#menu").css("opacity","100");
+	}
+});
+
+//		$(window).resize(function(){ //FIX THIS
+//			winWidth = $(window).width();
+//			if(winWidth <= 1080){
+//				mobileMode = true;
+//				$("#menu").css("margin-left","110vw");
+//				$("#menu").css("display","none");
+//				$("#menu").css("opacity","0");
+//			}else{
+//				mobileMode = false;
+//				$("#menu").css("margin-left","0px");
+//				$("#menu").css("display","flex");
+//				$("#menu").css("opacity","100");
+//			}
+//		});
+
+// Menu Modal from clicking on the Hamburger Menu
+$("#menu-hamburger").on("click",function(){
+	$(".menu-modal").css("display","block");
+	$('body').css("overflow", "hidden");
+	$("#vint-top").css("display","none");
+	$(".toggle-thumb").css("display","none");
+//			$("#banner").css("position","fixed");
+
+	$("#menu").css("margin-left","110vw");
+	$("#menu").css("display","flex");
+	$("#menu").css("opacity","0");
+	$("#menu").animate({
+		opacity: 1,
+		marginLeft: '40vw'
+	}, 400, "swing", function(){
+		//done
+	});
+});
+
+// Gallery Modal
+$(".galleryOver").on("click",function() {
+	var ind = $(this).parent("li").index();
+//			var txt = $(this).children(".galleryText").text();
+	var top = $(window).scrollTop() + ($(window).height() * (1/8));
+	var modal = $("#modal-list").children(".modal").filter(":nth-child("+(ind+1)+")");
+	$(modal).css("display","block");
+	$('body').css("overflow", "hidden");
+	$("#vint-top").css("display","none");
+	$(".toggle-thumb").css("display","none");
+
+	var modalchild = $(modal).children(".modal-content");
+	$(modalchild).css("opacity","0");
+	$(modalchild).css("top","-300px");
+	$(modalchild).animate({
+		opacity: 1,
+		top: top
+	}, 400, "swing", function(){
+		//done
+	});
+});
+
+// Resume Modal TEMP NOT WORKING
+$("#resume-btn").on("click",function() {
+	var top = $(window).scrollTop() + ($(window).height() * (1/8));
+	var modal = $(this).parent().siblings(".modal");
+	$(modal).css("display","block");
+	$('body').css("overflow", "hidden");
+	$("#vint-top").css("display","none");
+	$(".toggle-thumb").css("display","none");
+
+	var modalchild = $(modal).children(".modal-content");
+	$(modalchild).css("opacity","0");
+	$(modalchild).css("top","-300px");
+	$(modalchild).animate({
+		opacity: 1,
+		top: top
+	}, 400, "swing", function(){
+		//done
+	});
+});
+
+
+// Close the Modal with the X Button or my clicking outside the modal window
+$(".close").on("click",function() { // X Button
+	var ind = $(this).parent(".modal").index();
+	var modal = $(".modal").filter(":nth-child("+(ind+1)+")");
+	var modalchild = $(modal).children(".modal-content");
+	$(modalchild).css("opacity","0");
+	$(modalchild).css("top","-300px");
+
+	$(".modal").css("display", "none");
+	$('body').css("overflow", "auto");
+	$("#vint-top").css("display","block");
+	$(".toggle-thumb").css("display","flex");
+});
+$(window).click(function(e) { // Outside the window
+	if( $(e.target).hasClass("modal") ) {
+		var ind = $(e.target).index();
+		var modal = $(".modal").filter(":nth-child("+(ind+1)+")");
+		var modalchild = $(modal).children(".modal-content");
+		$(modalchild).css("opacity","0");
+		$(modalchild).css("top","-300px");
+
+		$(e.target).css("display", "none");
+		$('body').css("overflow", "auto");
+		$("#vint-top").css("display","block");
+		$(".toggle-thumb").css("display","flex");
+	}else if( $(e.target).hasClass("menu-modal") || (($(e.target).is("li")) && mobileMode) ) {
+		$("#menu").css("margin-left","110vw");
+		$("#menu").css("display","none");
+		$("#menu").css("opacity","0");
+
+		$(".menu-modal").css("display", "none");
+		$('body').css("overflow", "auto");
+		$("#vint-top").css("display","block");
+		$(".toggle-thumb").css("display","flex");
+//				$("#banner").css("position","relative");
+	}
+});
--- js/translation.js
+++ js/translation.js
@@ -1,7 +1,9 @@
 // JavaScript Document
 
+// EN Default
 translate("en");
 
+// Translation Toggle Detection
 $('.switch').children('input:checkbox').change(
 	function(){
 		if($(this).is(":checked")){
@@ -16,10 +18,22 @@ $('.switch').children('input:checkbox').change(
 
 //CONNECT THESE TWO
 
+// EN to JP Translation and Vice Versa
 function translate(lang){
 	langMode = lang; //for outside translate button toggle
 	if(lang === "jp"){
-		$("#menu").css("font-size","1.25em");
+		if(mobileMode){
+			$("#menu").css("font-size","1.25em");
+			$("#intro").css("font-size","2.25em");
+			$("#gallery").find("h1").css("font-size","2.25em");
+			$("#contact").css("font-size","2.25em");
+		}else{
+			$("#menu").css("font-size","1.25em");
+			$("#intro").css("font-size","2.25em");
+			$("#gallery").find("h1").css("font-size","2.25em");
+			$("#contact").css("font-size","2.25em");
+		}
+		
 		$("#menu").css("font-family","Toppan Bunkyu Midashi Gothic");
 		$("#menu").find("ul").css("margin-top","6px");
 		$("#menu").children("li").css("letter-spacing","0px");
@@ -30,7 +44,6 @@ function translate(lang){
 		$("#menu").find("li").eq(3).text("連絡先");
 		
 		$("#intro").html("<a style='color: #d1c55a; text-shadow: 0px;'>メ</a>ッセージ");
-		$("#intro").css("font-size","2.25em");
 		$("#intro").css("font-family","Toppan Bunkyu Midashi Gothic");
 		
 		$("#intro-content").find("p").eq(0).html("マティアス・ローレンス・A・ヴィリアリーノ <br> 大原情報ITクリエイター専門学校 <br> サン・カルロス大学");
@@ -47,7 +60,6 @@ function translate(lang){
 		$("#skills").find("h2").eq(3).siblings("p").text("創作活動、動画制作、音楽を聴くこと、絵を描くこと、キャラクターデザイン");
 		
 		$("#gallery").find("h1").html("<a style='color: #569bab; text-shadow: 0px;'>ポ</a>ートフォリオ");
-		$("#gallery").find("h1").css("font-size","2.25em");
 		$("#gallery").find("h1").css("font-family","Toppan Bunkyu Midashi Gothic");
 		
 		$(".galleryText").css("font-size","20px");
@@ -64,13 +76,49 @@ function translate(lang){
 		$(".gallery").find("li").eq(10).find(".galleryText").html("BToB アルミニウム");
 		$(".gallery").find("li").eq(11).find(".galleryText").html("プリントパック<br>年賀状2024辰年");
 		
+		$("#galleryModal11").find("h2").text("春秋会１００周年ロゴマーク");
+		$("#galleryModal1").find("h2").text("クールビズ");
+		$("#galleryModal2").find("h2").text("締めよう！シートベルト");
+		$("#galleryModal3").find("h2").text("動物病院チラシ");
+		$("#galleryModal4").find("h2").text("痴漢バッジ");
+		$("#galleryModal5").find("h2").text("インボイス制度ポスター");
+		$("#galleryModal6").find("h2").text("大阪医師会年賀状");
+		$("#galleryModal7").find("h2").text("大阪弁護士会マスコット");
+		$("#galleryModal8").find("h2").text("大原学園の新ロゴ");
+		$("#galleryModal9").find("h2").text("美容院チラシ");
+		$("#galleryModal10").find("h2").text("BToB アルミニウム");
+		$("#galleryModal12").find("h2").text("プリントパック年賀状2024辰年");
+		
+		$("#galleryModal11").find("p").text("弁理士春秋会の100周年記念のロゴマークです。この葉と花の花輪(リース)は上から下に春秋会の２つの季節(秋の試験合格祝賀、春の役員選挙)を示し、円形でサイクルと周年記念の意味を表すものです。");
+		$("#galleryModal1").find("p").text("クールビズは室温28°Cで快適に過ごせる服装で、ライフスタイルのキャンペーンを告知するポスターです。クールビズの意味が分かりやすくする為、文字をデザインして涼しい背景を制作しました。");
+		$("#galleryModal2").find("p").text("シートベルトを着用し安全な運転を促す農作業のポスターです。特別な色のシャツを着ている人々が手を繋ぐ事で、シートベルト、また農家のコミュニティーに安全を促がすと言う意味を表現しています。");
+		$("#galleryModal3").find("p").text("動物病院を宣伝するチラシです。目立たせる為に、面白い犬の画像をレイアウトしました。");
+		$("#galleryModal4").find("p").text("このバッジは痴漢を思いとどまらせるためのものです。痴漢をした結果、塀の中の人に入るというメッセージを込めています。");
+		$("#galleryModal5").find("p").text("このポスターは、インボイス制度に差し迫った事を企業に緊急に知らせるものです。作成したポスターの設定にはターゲットの企業がインボイス制度に入っていないので、ビジネスが破産していて、サプライチェーン(供給連鎖)が破綻している様子を表現しています。");
+		$("#galleryModal6").find("p").text("このポスターは大阪府医師会の正月のためのものです。新年の太陽と正月の餅つきを使うことで、癸卯を代表する月の兎を表現しています。");
+		$("#galleryModal7").find("p").text("大阪弁護士会のためのマスコットキャラクターです。このマスコットは桜草、大阪の漫才(ネクタイ、メガネ)、正義の秤、そしてダイナミックでフレンドリーなポーズの組み合わせです。");
+		$("#galleryModal8").find("p").text("これは私の学校のロゴデザインコンテストに応募したものです。傾けたOpportunity(チャンス)のOに越える上向きの線と言うロゴマークです。");
+		$("#galleryModal9").find("p").text("特別メニューと割引が何よりも強調している高級メンズヘアサロンのチラシです。");
+		$("#galleryModal10").find("p").text("アルミニウムの再利用を特化した「UACJ」と言う日本の製造業界の会社のために作ったBtoBをテーマにしたポスターです。アルミ製の物が同じ一本の缶から出て来ています。");
+		$("#galleryModal12").find("p").text("プリントパックが運営する2024年の木竜の年賀状コンテストへの応募作品です。これはラッキーカラーと吉方位を使った書道の「辰」と新年の日の出を組み合わせたものです。");
+		
 		$("#contact").html("<a style='color: #D35321; text-shadow: 0px;'>連</a>絡先");
-		$("#contact").css("font-size","2.25em");
 		$("#contact").css("font-family","Toppan Bunkyu Midashi Gothic");
 		
 		$("#vint-top").find("figure").html("<img src='images/vintage_top_jp.png' alt=''>");
 	}else{
-		$("#menu").css("font-size","1.5em");
+		if(mobileMode){
+			$("#menu").css("font-size","1.5em");
+			$("#intro").css("font-size","3em");
+			$("#gallery").find("h1").css("font-size","3em");
+			$("#contact").css("font-size","3em");
+		}else{
+			$("#menu").css("font-size","1.5em");
+			$("#intro").css("font-size","2.5em");
+			$("#gallery").find("h1").css("font-size","2.25em");
+			$("#contact").css("font-size","2.25em");
+		}
+		
 		$("#menu").css("font-family","Barlow Condensed");
 		$("#menu").find("ul").css("margin-top","6px");
 		$("#menu").children("li").css("letter-spacing","2px");
@@ -81,7 +129,6 @@ function translate(lang){
 		$("#menu").find("li").eq(3).text("REACH OUT");
 		
 		$("#intro").html("<a style='color: #d1c55a; text-shadow: 0px;'>A</a>BOUT ME");
-		$("#intro").css("font-size","2.5em");
 		$("#intro").css("font-family","Barlow Condensed");
 		
 		$("#intro-content").find("p").eq(0).html("Matthias Laurence A. Villarino <br> Ohara Jouhou IT Creator Senmon Gakkou <br> University of San Carlos");
@@ -98,7 +145,6 @@ function translate(lang){
 		$("#skills").find("h2").eq(3).siblings("p").text("Creative hobbies, making videos, listening to music, drawing, character design");
 		
 		$("#gallery").find("h1").html("<a style='color: #569bab; text-shadow: 0px;'>P</a>ORTFOLIO");
-		$("#gallery").find("h1").css("font-size","2.25em");
 		$("#gallery").find("h1").css("font-family","Barlow Condensed");
 		
 		$(".galleryText").css("font-size","30px");
@@ -115,8 +161,33 @@ function translate(lang){
 		$(".gallery").find("li").eq(10).find(".galleryText").html("BToB Aluminium Poster");
 		$(".gallery").find("li").eq(11).find(".galleryText").html("Printpac New Year's Card<br>Year of the Dragon 2024");
 		
+		$("#galleryModal11").find("h2").text("Shunjukai 100th Anniversary Logo");
+		$("#galleryModal1").find("h2").text("Cool Biz");
+		$("#galleryModal2").find("h2").text("Farmwork Safety");
+		$("#galleryModal3").find("h2").text("Veterinary Clinic Flyer");
+		$("#galleryModal4").find("h2").text("Anti-Molester Badge");
+		$("#galleryModal5").find("h2").text("Invoice System Poster");
+		$("#galleryModal6").find("h2").text("Osaka Medical Association New Year's Card");
+		$("#galleryModal7").find("h2").text("Osaka Bar Association Mascot");
+		$("#galleryModal8").find("h2").text("Updated Ohara Logo");
+		$("#galleryModal9").find("h2").text("Hair Salon Flyer");
+		$("#galleryModal10").find("h2").text("BToB Aluminium Poster");
+		$("#galleryModal12").find("h2").text("Printpac New Year's Card Year of the Dragon 2024");
+		
+		$("#galleryModal11").find("p").text("ass we can");
+		$("#galleryModal1").find("p").text("ass we can");
+		$("#galleryModal2").find("p").text("ass we can");
+		$("#galleryModal3").find("p").text("ass we can");
+		$("#galleryModal4").find("p").text("ass we can");
+		$("#galleryModal5").find("p").text("ass we can");
+		$("#galleryModal6").find("p").text("ass we can");
+		$("#galleryModal7").find("p").text("ass we can");
+		$("#galleryModal8").find("p").text("ass we can");
+		$("#galleryModal9").find("p").text("ass we can");
+		$("#galleryModal10").find("p").text("ass we can");
+		$("#galleryModal12").find("p").text("ass we can");
+		
 		$("#contact").html("<a style='color: #D35321; text-shadow: 0px;'>R</a>EACH OUT");
-		$("#contact").css("font-size","2.25em");
 		$("#contact").css("font-family","Barlow Condensed");
 		
 		$("#vint-top").find("figure").html("<img src='images/vintage_top.png' alt=''>");
